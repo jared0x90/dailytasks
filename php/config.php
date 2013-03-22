0c710e3015494ae0a7538af8839da81b69856b4f
Jared De Blander <jared0x90@gmail.com>
2013-03-22 00:57:39 +0000

    Mongo configuration

--- php/config.php
+++ php/config.php
@@ -1,7 +1,13 @@
 <?php
 
-# $url = 'mongodb://inkly:archon23051@dbh74.mongolab.com:27747/inkly';
 
-echo $url;
+# Mongo Database Configuration
 
-?>
+define('MONGO_URL',  'mongodb://inkly:archon23051@dbh74.mongolab.com:27747/inkly');
+
+define('MONGO_HOST', 'dbh74.mongolab.com');
+define('MONGO_USER', 'inkly');
+define('MONGO_PASS', 'archon23051');
+define('MONGO_PORT', '27747');
+define('MONGO_PATH', '/inkly');
+
