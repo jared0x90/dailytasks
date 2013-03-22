9a46e4e2920df2d788f67d8515a7ca2ae6fc94bf
Jared De Blander <jared0x90@gmail.com>
2013-03-22 01:16:39 +0000

    Small style changes. Lorem ipsum text

--- php/views/default.php
+++ php/views/default.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Inkly's - Your source for toner and ink!</title>
+    <title>Inkly's - Ink, toner and a better world</title>
     <link rel="stylesheet" href="css/style.css">
     <link href='http://fonts.googleapis.com/css?family=Gabriela' rel='stylesheet' type='text/css'>
   </head>
@@ -10,15 +10,27 @@
     <!-- page content -->
     <div class="header">
       <h1>Inkly's</h1>
-      <h2>Your source for toner &amp; ink</h2>
+      <h2>Ink, toner &amp; a better world</h2>
     </div>
     <div class="navigation">
-      <a href="">Ink</a> |
-      <a href="">Toner</a>
+      <a href="">HP Ink</a> |
+      <a href="">Canon Ink</a> |
+      <a href="">Brother Ink</a> |
+      <a href="">HP Toner</a>
     </div>
     <hr>
     <div class="main">
-      Welcome to Inkly's.
+      <p>Welcome to Inkly's.</p>
+      
+      <!-- lorem ipsum -->
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper facilisis ante, eget lobortis nisl aliquam vitae. Nulla eros leo, lacinia eget feugiat vitae, pretium quis purus. Donec commodo adipiscing elementum. Mauris ultrices pretium felis, quis accumsan est sagittis eget. Pellentesque venenatis, erat facilisis blandit molestie, augue purus pretium quam, sed aliquam ante orci vel nulla. In lobortis, sapien et dictum imperdiet, justo diam mollis purus, gravida tincidunt velit sem eget dui. In fringilla ligula ac augue malesuada dapibus. Nunc a nisi non nisl ultrices cursus id et libero. Phasellus in ligula eu nunc lacinia convallis. Nulla facilisi. Quisque eu tellus metus.</p>
+
+      <p>Vestibulum ultricies justo et enim volutpat sit amet dictum augue faucibus. Maecenas lobortis tellus sit amet lacus pulvinar vitae suscipit ligula semper. Aenean vel odio quis augue elementum iaculis quis ac lacus. Donec dignissim turpis a tellus aliquet tempor. Nam pellentesque, nisi malesuada rutrum tempus, erat lorem semper mauris, et eleifend odio justo vel diam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus lorem risus, sagittis a pharetra ac, rutrum quis dolor. Duis quis mi nibh. Proin varius leo vel velit rutrum tempus interdum dui feugiat. Pellentesque gravida vestibulum lacus, consectetur rhoncus urna laoreet eu. Phasellus dolor sapien, interdum ac posuere ac, elementum posuere ante. Duis nec sagittis sapien.</p>
+
+      <p>Nunc malesuada orci sed tortor pulvinar euismod hendrerit massa sodales. Aliquam molestie volutpat sapien, in pretium purus fringilla ac. Integer ligula mauris, tempor eu imperdiet quis, pharetra et enim. Integer justo lacus, convallis nec vulputate tristique, auctor eu lectus. In pretium nisi lectus, pellentesque dignissim odio. Etiam quis odio odio. Praesent et egestas ipsum. Nunc sollicitudin, sem at aliquam eleifend, enim sapien ullamcorper mi, ac tincidunt lorem massa sit amet libero. Ut id orci augue, vitae facilisis sapien. Sed eget lectus eu augue fringilla sodales.</p>
+      
+      
     </div>
     <hr>
     <div class="footer">
